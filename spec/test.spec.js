6760fee434150c6992d38a393b241601f6155380
Nothile Ngcobo <nothilengcobo@nothiles-MacBook-Pro.local>
2019-11-26 11:20:42 +0200

    fixed negatives

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -22,18 +22,17 @@ describe("function takes a string",function() {
      it('should allow for different delimeters', function() {
          expect(calculator.add("//;\n1;2#%6@!fd")).toEqual(9);
      });
-// whatever dekimeter you add should not affect your output
+// whatever delimeter you add should not affect your output
      it('should add more than 4 numeric values', function () {
          expect(calculator.add("//;\n1;3#%4@!1f1d")).toEqual(10);
      });
     
     it("should give an error msg negatives not allowed", function(){
-        expect(calculator.add("//;\n3,3")).toEqual(6);
-
+        expect(function() {calculator.add("//;\n-22,3")}).toThrowError("Negatives are not allowed -22 ");
     });
 
     
 });
 
-//testing 
-console.log(calculator.add("2,@4"));
+// for testing 
+console.log(calculator.add("2,@4,-2"));
--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -5,17 +5,30 @@
     let output = string.replace(/[^\d.-]/g, '');
         let checkNegative = parseInt(output);
 
+    // try{
+
+        let  negs = "";
+
+        for (var i = 0; i <= string.length; i++){
+            if(string[i] === "-" && !isNaN(string[i+1])){
+                negs += "-" + string[i + 1] + " ";
+            }
+        }
+
+        if(negs != ""){
+            throw new Error("Negatives are not allowed " + negs);
+        }
+
         if(string == "") {
             return "0"; // check the string is not empty
-        } else if (checkNegative < 0) {
-            return "negatives not allowed " + output;
-
         } else {
             for(let i =0; i < output.length; i++){
                 let changed = parseInt(output[i]);
                 added = added + changed;
             }return added;
         }
+    // }catch(Error){
+    //     return Error
+    // }
     }
-
 }
